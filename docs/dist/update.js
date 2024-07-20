9aeb00df25787e9995e15d8fd4c244b1b84b8883
simetrio <simetrio@yandex.com>
2024-07-20 08:39:32 +0500

    Initial commit

---
+++ docs/dist/update.js
@@ -0,0 +1,34 @@
+let mainJsVersion = null
+
+const start = "src=\"/main.js?"
+const end = "\""
+
+const checkVersion = async () => {
+    const result = await fetch(`/?v=${new Date().getTime()}`)
+    const html = await result.text()
+
+    let newMainJsVersion = html.substring(html.indexOf(start) + start.length)
+    newMainJsVersion = newMainJsVersion.substring(0, newMainJsVersion.indexOf(end))
+
+    console.log(`Check update version. Current version ${mainJsVersion}. New version ${newMainJsVersion}`)
+
+    if (!mainJsVersion) {
+        console.log("Current version is not exists. Set it")
+        mainJsVersion = newMainJsVersion
+        return
+    }
+
+    if (mainJsVersion == newMainJsVersion) {
+        console.log("Versions are equals. Nothing doing")
+        return
+    }
+
+    console.log("Versions are not equals. Reload page")
+    location.reload()
+}
+
+setInterval(async () => {
+    await checkVersion()
+}, 60000)
+
+checkVersion()
---
+++ docs/readme.txt
@@ -0,0 +1,4 @@
+Для запуска приложения необходимо:
+- установить Node.js (https://nodejs.org)
+- запустить start.bat для Windows или start.sh для Linux
+- открыть в браузере сайт http://localhost:4000
