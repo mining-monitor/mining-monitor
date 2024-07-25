b78c891adf11c4fd09149ab1e16c9bd678127d7f
simetrio <simetrio@yandex.com>
2024-07-25 16:04:05 +0500

    Реализую оставшиеся методы Jasminer

--- src/emulator/jasminer/controller.ts
+++ src/emulator/jasminer/controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express"
 
 export const controller = {
-    getPools: (_: Request, response: Response) => {
+    status: async (_: Request, response: Response) => {
+        await sleep(random(500))
+
         response.send({
             ...minerInfo,
             summary: {
@@ -9,6 +11,37 @@ export const controller = {
                 rt: `19${random(99)}.15 MH/s`,
             }
         })
+    },
+    reboot: async (_: Request, response: Response) => {
+        await sleep(random(1000))
+
+        response.send("Ok")
+    },
+    getPools: async (_: Request, response: Response) => {
+        await sleep(random(500))
+
+        response.send(pools)
+    },
+    setPools: async (request: Request, response: Response) => {
+        await sleep(random(2000))
+
+        pools.pools[0].url = request.body.pool1url
+        pools.pools[0].user = request.body.pool1user
+        pools.pools[0].pass = request.body.pool1pw
+
+        pools.pools[1].url = request.body.pool2url
+        pools.pools[1].user = request.body.pool2user
+        pools.pools[1].pass = request.body.pool2pw
+
+        pools.pools[2].url = request.body.pool3url
+        pools.pools[2].user = request.body.pool3user
+        pools.pools[2].pass = request.body.pool3pw
+
+        pools.protocol = request.body.protocol
+        pools.coin = request.body.coin
+        pools.work_pattern = request.body.work_pattern
+
+        response.send("Ok")
     },
 }
 
@@ -48,4 +81,31 @@ const minerInfo = {
     }
 }
 
+const pools = {
+    protocol: "getwork",
+    coin: "etc_zil",
+    work_pattern: "2",
+    pools: [
+        {
+            url: "http://someUrl_1",
+            user: "someUser.jas1",
+            pass: "1",
+        },
+        {
+            url: "http://someUrl_2",
+            user: "someUser.jas2",
+            pass: "1",
+        },
+        {
+            url: "http://someUrl_3",
+            user: "someUser.jas1",
+            pass: "1",
+        },
+    ],
+}
+
 const random = (max: number) => Math.floor(Math.random() * max)
+
+export const sleep = (ms: number) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
--- src/emulator/jasminer/index.ts
+++ src/emulator/jasminer/index.ts
@@ -3,7 +3,12 @@ import { controller } from "./controller"
 
 const app = express()
 
-app.get("/cgi-bin/minerStatus_all.cgi", controller.getPools)
+app.use(express.urlencoded())
+
+app.get("/cgi-bin/minerStatus_all.cgi", controller.status)
+app.get("/cgi-bin/reboot.cgi", controller.reboot)
+app.get("/cgi-bin/get_pools.cgi", controller.getPools)
+app.post("/cgi-bin/set_pools.cgi", controller.setPools)
 
 app.listen(4010)
 
