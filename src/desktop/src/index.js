f175700f962f9d396b0e366f67d49b62c30e2226
simetrio <simetrio@yandex.com>
2024-07-30 09:01:14 +0500

    Устанавливаю ts

--- src/desktop/src/index.js
+++ src/desktop/src/index.js
@@ -1,49 +1,43 @@
-import { app, BrowserWindow } from "electron"
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const electron_1 = require("electron");
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) { // eslint-disable-line global-require
-  app.quit()
+    electron_1.app.quit();
 }
-
 // Keep a global reference of the window object, if you don"t, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-
+let mainWindow = null;
 const createWindow = () => {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({})
-  mainWindow.maximize()
-
-  // and load the index.html of the app.
-  mainWindow.loadURL("http://localhost:4000")
-
-  // Emitted when the window is closed.
-  mainWindow.on("closed", () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
-}
-
+    // Create the browser window.
+    mainWindow = new electron_1.BrowserWindow({});
+    mainWindow.maximize();
+    // and load the index.html of the app.
+    mainWindow.loadURL("http://localhost:4000");
+    // Emitted when the window is closed.
+    mainWindow.on("closed", () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
+};
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow)
-
+electron_1.app.on("ready", createWindow);
 // Quit when all windows are closed.
-app.on("window-all-closed", () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
-    app.quit()
-  }
-})
-
-app.on("activate", () => {
-  // On OS X it"s common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+electron_1.app.on("window-all-closed", () => {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== "darwin") {
+        electron_1.app.quit();
+    }
+});
+electron_1.app.on("activate", () => {
+    // On OS X it"s common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
