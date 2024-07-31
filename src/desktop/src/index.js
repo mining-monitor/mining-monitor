503c381c7bb5d6bc17ea6bf5bb4544c76c9bdcca
simetrio <simetrio@yandex.com>
2024-07-31 12:35:32 +0500

    Переношу приложение

--- src/desktop/src/index.js
+++ src/desktop/src/index.js
@@ -1,51 +1,114 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('node:path');
+const { app, BrowserWindow, Tray, Menu } = require("electron")
+const path = require("node:path")
+const { webServerRunner } = require("./webServerRunner")
+const { log } = require("./log")
+const { environment } = require("./environment")
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-  app.quit();
+let mainWindow = null
+let isShow = true
+let isQuit = false
+
+if (require("electron-squirrel-startup")) {
+  app.quit()
 }
 
 const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+  mainWindow = new BrowserWindow({
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
     },
-  });
-
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow();
-
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+  })
+log.info(process.env.ELECTRON_IS_DEV)
+  mainWindow.maximize()
+  mainWindow.loadFile(path.join(__dirname, "index.html"))
+  mainWindow.on("close", (event) => {
+    if (!isQuit) {
+      event.preventDefault()
+      hideWindow()
     }
-  });
-});
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
+    return false;
+  })
+
+  mainWindow.on("minimize", (event) => {
+    event.preventDefault()
+    hideWindow()
+  })
+
+  createTray()
+}
+
+const createTray = () => {
+  const tray = new Tray(path.join(__dirname, "favicon.png"))
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Открыть в браузере",
+      click: openBrowser,
+    },
+    {
+      label: "Открыть",
+      click: showWindow,
+    },
+    {
+      label: "Завершить",
+      click: quit,
+    },
+  ])
+
+  tray.setToolTip("Майнинг монитор")
+  tray.setContextMenu(contextMenu)
+
+  tray.on("click", toogleWindow)
+}
+
+const toogleWindow = () => {
+  if (isShow) {
+    mainWindow.hide()
+    isShow = false
+  } else {
+    mainWindow.show()
+    isShow = true
   }
-});
+}
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+const showWindow = () => {
+  mainWindow.show()
+  isShow = true
+}
+
+const hideWindow = () => {
+  mainWindow.hide()
+  isShow = false
+}
+
+const quit = () => {
+  isQuit = true
+  app.quit()
+}
+
+const openBrowser = () => {
+  environment.commandLine("start", "http://localhost:4000")
+  environment.commandLine("xdg-open", "http://localhost:4000")
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    quit()
+  }
+})
+
+webServerRunner.run(() => {
+  log.info("Load url http://localhost:4000")
+  mainWindow.loadURL("http://localhost:4000")
+})
