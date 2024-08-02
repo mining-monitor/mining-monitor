6e03eddcc37e7c9f1d3df2ec1d3e98788578f5fa
simetrio <simetrio@yandex.com>
2024-08-02 18:20:45 +0500

    Добавляю контент тайп

--- src/server/src/controllers/staticController.ts
+++ src/server/src/controllers/staticController.ts
@@ -3,20 +3,22 @@ import { environment } from "../lib/environment"
 
 export const staticController = {
     index: async (request: Request, response: Response) => {
-        await load(request, response, "/dist/index.html")
+        await load(request, response, "/dist/index.html", "text/html")
     },
     mainJs: async (request: Request, response: Response) => {
-        await load(request, response, "/dist/main.js")
+        await load(request, response, "/dist/main.js", "application/javascript")
     },
     updateJs: async (request: Request, response: Response) => {
-        await load(request, response, "/dist/update.js")
+        await load(request, response, "/dist/update.js", "application/javascript")
     },
     favicon: async (request: Request, response: Response) => {
-        await load(request, response, "/dist/favicon.ico")
+        await load(request, response, "/dist/favicon.ico", "image/vnd.microsoft.icon")
     },
 }
 
-const load = async (request: Request, response: Response, file: string) => {
+const load = async (request: Request, response: Response, file: string, contentType: string) => {
+    response.set("Content-Type", contentType)
+   
     if (environment.isDebug()) {
         response.sendFile(__dirname + file)
     }
