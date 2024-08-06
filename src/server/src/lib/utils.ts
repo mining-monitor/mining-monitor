268fe76706f324d59da867220e613cd38ef3c414
simetrio <simetrio@yandex.com>
2024-08-06 09:40:30 +0500

    Добавил логирования в уведомления

--- src/server/src/lib/utils.ts
+++ src/server/src/lib/utils.ts
@@ -1,3 +1,21 @@
+import { log } from "./log";
+
 export const sleep = (ms: number) => {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export const safeExecute = async (action: () => Promise<void>) => {
+    try {
+        await action()
+    } catch(error) {
+        log.error(error)
+    }
+}
+
+export const safeExecuteSync = (action: () => void) => {
+    try {
+        action()
+    } catch(error) {
+        log.error(error)
+    }
 }
--- src/server/src/schedulers/notificationsScheduler.ts
+++ src/server/src/schedulers/notificationsScheduler.ts
@@ -4,6 +4,7 @@ import { Settings } from "../../../lib/settings"
 import { telegram } from "../../../lib/telegram"
 import { date } from "../../../lib/date"
 import { log } from "../lib/log"
+import { safeExecuteSync } from "../lib/utils"
 
 interface NotificationData {
     turnOffDate: Date | null,
@@ -14,57 +15,63 @@ let lastNotify: Date | null = null
 
 export const notificationsScheduler = {
     work: () => {
-        const currentSettings = settings.get()
+        safeExecuteSync(() => {
+            const currentSettings = settings.get()
 
-        currentSettings.miners.forEach(x => {
-            const value = minerInfos.get(x.ip)
+            currentSettings.miners.forEach(x => {
+                const value = minerInfos.get(x.ip)
 
-            if (!notificationsData.get(x.ip)) {
-                notificationsData.set(x.ip, { turnOffDate: null })
-            }
+                if (!notificationsData.get(x.ip)) {
+                    notificationsData.set(x.ip, { turnOffDate: null })
+                    log.info("notificationsScheduler", "addNewIpToData", x.ip)
+                }
 
-            if (!value && !notificationsData.get(x.ip)!.turnOffDate) {
-                notificationsData.set(x.ip, { turnOffDate: new Date() })
-            }
+                if (!value && !notificationsData.get(x.ip)!.turnOffDate) {
+                    notificationsData.set(x.ip, { turnOffDate: new Date() })
+                    log.info("notificationsScheduler", "minerTurnOff", x.ip)
+                }
 
-            if (value && notificationsData.get(x.ip)!.turnOffDate) {
-                notificationsData.set(x.ip, { turnOffDate: null })
-            }
-        })
+                if (value && notificationsData.get(x.ip)!.turnOffDate) {
+                    notificationsData.set(x.ip, { turnOffDate: null })
+                    log.info("notificationsScheduler", "minerTurnOn", x.ip)
+                }
+            })
 
-        const ips = currentSettings.miners.map(x => x.ip)
-        Array.from(notificationsData.keys()).forEach(x => {
-            if (ips.indexOf(x) !== -1) {
+            const ips = currentSettings.miners.map(x => x.ip)
+            Array.from(notificationsData.keys()).forEach(x => {
+                if (ips.indexOf(x) !== -1) {
+                    return
+                }
+
+                notificationsData.delete(x)
+                log.info("notificationsScheduler", "deleteIpFromData", x)
+            })
+
+            if (!currentSettings.notifications.enabled) {
                 return
             }
 
-            notificationsData.delete(x)
-        })
+            notificationsData.forEach(async (value, key) => {
+                if (!value.turnOffDate) {
+                    return
+                }
 
-        if (!currentSettings.notifications.enabled) {
-            return
-        }
+                const fromLastNotify = !!lastNotify
+                    ? new Date().getTime() - lastNotify.getTime()
+                    : Number.MAX_VALUE
 
-        notificationsData.forEach(async (value, key) => {
-            if (!value.turnOffDate) {
-                return
-            }
-
-            const fromLastNotify = !!lastNotify
-                ? new Date().getTime() - lastNotify.getTime()
-                : Number.MAX_VALUE
-
-            const fromTurnOff = new Date().getTime() - value.turnOffDate.getTime()
-            if (fromTurnOff > 5 * 60 * 1000 && fromLastNotify > 15 * 60 * 1000) {
-                await notify(key, value.turnOffDate, currentSettings)
-                lastNotify = new Date()
-            }
+                const fromTurnOff = new Date().getTime() - value.turnOffDate.getTime()
+                if (fromTurnOff > 5 * 60 * 1000 && fromLastNotify > 15 * 60 * 1000) {
+                    await notify(key, value.turnOffDate, currentSettings)
+                    lastNotify = new Date()
+                }
+            })
         })
     }
 }
 
 const notify = async (ip: string, turnOffDate: Date, currentSettings: Settings) => {
-    log.info("notify", ip, turnOffDate)
+    log.info("notificationsScheduler", "notify", ip, turnOffDate)
 
     const miner = currentSettings.miners.find(x => x.ip === ip)!
 
