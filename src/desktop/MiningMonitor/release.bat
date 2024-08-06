519bc27a32b7abecef8aa2db5d6e2e2d61ea7d3d
Roman Puzanov <puzanov.rs@skbkontur.ru>
2024-08-06 11:22:17 +0500

    Новая версия десктопного приложения

--- src/desktop/MiningMonitor/release.bat
+++ src/desktop/MiningMonitor/release.bat
@@ -1,3 +1,5 @@
 ﻿# release
 dotnet build -c Release -o "./publish"
-%userprofile%/.nuget/packages/clowd.squirrel/2.11.1/tools/Squirrel.exe pack --framework net6 --packId "MiningMonitor" --packVersion "1.0.3" --packTitle "MiningMonitor" --packAuthors "MiningMonitor" --packDirectory "./publish" --releaseDir "../../../docs/desktop" --icon "./favicon.ico" --setupIcon "./favicon.ico" --splashImage "install.gif"  --splashImage "install.gif" --noDelta
+%userprofile%/.nuget/packages/clowd.squirrel/2.11.1/tools/Squirrel.exe pack --framework net6 --packId "MiningMonitor" --packVersion "1.0.4" --packTitle "MiningMonitor" --packAuthors "MiningMonitor" --packDirectory "./publish" --releaseDir "../../../docs/desktop" --icon "./favicon.ico" --setupIcon "./favicon.ico" --splashImage "install.gif"  --splashImage "install.gif" --noDelta
+
+pause
